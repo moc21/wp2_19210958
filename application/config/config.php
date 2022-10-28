36a3fc334af474e52867bce7f762f2f12e3f2704
moc21 <rendydwinugroho94@gmail.com>
2022-10-28 21:15:59 +0700

    tugas uts

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined("BASEPATH") OR exit("No direct script access allowed");
 | a PHP script and you can easily do that on your own.
 |
 */
+
 //$config['base_url'] = 'http://localhost/wp2-19210958';
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
